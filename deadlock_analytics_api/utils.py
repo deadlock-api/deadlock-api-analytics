aef42449b66124cea0ee92baf1f98d5c8696e2a9
Manuel Raimann <raimannma@outlook.de>
2024-10-13 03:12:09 +0200

    Allow comments in api keys

--- deadlock_analytics_api/utils.py
+++ deadlock_analytics_api/utils.py
@@ -8,6 +8,7 @@ api_key_query = APIKeyQuery(name="api_key", auto_error=False)
 async def get_api_key(api_key_query: str = Security(api_key_query)):
     with open("api_keys.txt") as f:
         available_api_keys = f.read().splitlines()
+        available_api_keys = [a.split("#")[0].strip() for a in available_api_keys]
     if api_key_query in available_api_keys:
         return api_key_query
     raise HTTPException(status_code=HTTP_403_FORBIDDEN)
@@ -16,6 +17,7 @@ async def get_api_key(api_key_query: str = Security(api_key_query)):
 async def get_internal_api_key(api_key_query: str = Security(api_key_query)):
     with open("internal_api_keys.txt") as f:
         available_api_keys = f.read().splitlines()
+        available_api_keys = [a.split("#")[0].strip() for a in available_api_keys]
     if api_key_query in available_api_keys:
         return api_key_query
     raise HTTPException(status_code=HTTP_403_FORBIDDEN)
