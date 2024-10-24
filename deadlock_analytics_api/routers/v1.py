26890edd5f458b79a22d5014f1d0771393cb8b7e
Manuel Raimann <raimannma@outlook.de>
2024-10-24 17:15:57 +0200

    Set rate limit for mmr-history

--- deadlock_analytics_api/routers/v1.py
+++ deadlock_analytics_api/routers/v1.py
@@ -238,7 +238,7 @@ Ranks update in 1min intervals.
 
 As the calculation uses the match_score, it updates when a player starts a new match and will always be one match behind the real rank.
 """,
-    summary="RateLimit: 100req/s",
+    summary="RateLimit: 100req/s & 1000req/10min",
 )
 def get_player_mmr_history(
     req: Request,
@@ -251,7 +251,7 @@ def get_player_mmr_history(
         req,
         res,
         "/v1/players/{account_id}/mmr-history",
-        [RateLimit(limit=100, period=1)],
+        [RateLimit(limit=100, period=1), RateLimit(limit=1000, period=600)],
         [RateLimit(limit=1000, period=1)],
     )
     res.headers["Cache-Control"] = "public, max-age=300"
