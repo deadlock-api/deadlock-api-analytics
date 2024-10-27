a1a85a5335ed2c5fed3e65a0f39bdf14e8c2c918
Manuel Raimann <raimannma@outlook.de>
2024-10-27 02:29:47 +0100

    Update recent matches

--- deadlock_analytics_api/routers/internal.py
+++ deadlock_analytics_api/routers/internal.py
@@ -19,7 +19,7 @@ def get_recent_matches(
     query = """
     SELECT DISTINCT match_id
     FROM finished_matches
-    WHERE start_time < now() - INTERVAL '1 hour'
+    WHERE start_time < now() - INTERVAL '2 hours'
         AND start_time > now() - INTERVAL '7 days'
         AND match_id NOT IN (SELECT match_id FROM match_salts)
     ORDER BY start_time DESC
