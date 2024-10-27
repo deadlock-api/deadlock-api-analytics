48ad1deac8c150d97dbf80e6e7fae4eca990d955
Manuel Raimann <raimannma@outlook.de>
2024-10-27 11:44:10 +0100

    Track failed matches

--- deadlock_analytics_api/routers/internal.py
+++ deadlock_analytics_api/routers/internal.py
@@ -2,7 +2,7 @@ from deadlock_analytics_api import utils
 from deadlock_analytics_api.globs import CH_POOL
 from fastapi import APIRouter, Depends
 from fastapi.openapi.models import APIKey
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from starlette.exceptions import HTTPException
 from starlette.responses import JSONResponse, Response
 
@@ -32,9 +32,10 @@ def get_recent_matches(
 
 class MatchSalts(BaseModel):
     match_id: int
-    cluster_id: int
-    metadata_salt: int
-    replay_salt: int
+    cluster_id: int | None = Field(None)
+    metadata_salt: int | None = Field(None)
+    replay_salt: int | None = Field(None)
+    failed: bool | None = Field(None)
 
 
 @router.get("/match-salts")
@@ -44,7 +45,7 @@ def get_match_salts(
     response.headers["Cache-Control"] = "private, max-age=1200"
     print(f"Authenticated with API key: {api_key}")
     query = """
-    SELECT match_id, cluster_id, metadata_salt, replay_salt
+    SELECT match_id, cluster_id, metadata_salt, replay_salt, failed
     FROM match_salts
     """
     with CH_POOL.get_client() as client:
@@ -57,6 +58,7 @@ def get_match_salts(
             cluster_id=row[1],
             metadata_salt=row[2],
             replay_salt=row[3],
+            failed=row[4],
         )
         for row in results
     ]
@@ -70,10 +72,10 @@ def post_match_salts(
 ) -> JSONResponse:
     response.headers["Cache-Control"] = "private, max-age=60"
     print(f"Authenticated with API key: {api_key}")
-    query = """
-    INSERT INTO match_salts (match_id, cluster_id, metadata_salt, replay_salt)
-    VALUES (%(match_id)s, %(cluster_id)s, %(metadata_salt)s, %(replay_salt)s)
-    """
+    if match_salts.failed:
+        query = "INSERT INTO match_salts (match_id, failed) VALUES (%(match_id)s, TRUE)"
+    else:
+        query = "INSERT INTO match_salts (match_id, cluster_id, metadata_salt, replay_salt) VALUES (%(match_id)s, %(cluster_id)s, %(metadata_salt)s, %(replay_salt)s)"
     with CH_POOL.get_client() as client:
         client.execute(query, match_salts.model_dump())
     return JSONResponse(content={"success": True})
